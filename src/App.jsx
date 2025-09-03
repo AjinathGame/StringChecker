829cf9012ae49f4d7c1df1808302c9ec6745f7b8
Ajinath-Game <gameajinath4@gmail.com>
2025-09-03 19:13:25 +0530

    Create StringChecker

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { use, useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [Leftstr , setLeftStr] = useState("");
+  const [Rightstr , setRightStr] = useState("");
+  const [message, setMessage] = useState("");
+
+
+  const checkstr = ()=>{
+    if(Leftstr === Rightstr){
+      setMessage("Strings are Same");
+    }else{
+      setMessage("Strings are Not Same");
+    }
+  }
+
+  useEffect(()=>{
+    checkstr();
+  }, [Leftstr, Rightstr])
+
+
+  return (
+    <>
+        <h1>Text Checker</h1>
+        <div>
+          <textarea onChange={(e)=>setLeftStr(e.target.value)} value={Leftstr}/>
+          <textarea onChange={(e)=>setRightStr(e.target.value)} value={Rightstr}/>
+        </div>
+        <div>
+          <p>{message}</p>
+        </div>
+
+    </>
+
+
+  )
+}
+
+export default App
